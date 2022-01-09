9d9334c67158b835958d6b770b0e3887b158406c
misterZink <heidarirobin@protonmail.com>
2022-01-09 15:15:34 +0100

    Init

---
+++ src/main/java/com/example/personsrest/domain/PersonRepository.java
@@ -0,0 +1,20 @@
+package com.example.personsrest.domain;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PersonRepository {
+    Optional<Person> findById(String id);
+    List<Person> findAll();
+    Page<Person> findAllByNameContainingOrCityContaining(String name, String city, Pageable pageable);
+
+    void deleteAll();
+
+    Person save(Person person);
+
+    void delete(String id);
+}
